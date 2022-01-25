28c04e9e3512f6934f3e062e5816295d908e30c4
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-01-25 12:41:28 -0700

    Better organization, added pressure options

--- tests/gen_ideal_test.py
+++ tests/gen_ideal_test.py
@@ -18,25 +18,40 @@ class IdealTest:
     n_hills           = 1.0      # number of hills across the domain
 
     def __init__(self, nz=10, nx=2, ny=2, n_hills=1.0):
-        rh = 0.9
-        u_test_val = 0.5
-        v_test_val = 0.5
+        mixing_ratio = 0.01 # water vapor
+        u_test_val = 0.0
+        v_test_val = 0.0
         w_test_val = 0.0
-        water_vapor_test_val = 0.001
+        water_vapor_test_val = 0.000
         theta_test_val = 300.0
+        qv_val = mixing_ratio
 
+        # --- choose function for creating pressure ---
+        pressure_func = 'calc_pressure_from_sea'
+        # pressure_func = 'calc_pressure_dz_iter'
+        # pressure_func = 'calc_pressure_1m_iter'
+
+        # double check all passed variable get used
         self.forcing = fc.Forcing(nz, nx, ny, self.sealevel_pressure,
-                                  rh, u_test_val, v_test_val, w_test_val,
+                                  u_test_val, v_test_val, w_test_val,
                                   water_vapor_test_val, theta_test_val,
-                                  dz_value=self.dz_value)
+                                  dz_value=self.dz_value, qv_val=qv_val,
+                                  # weather_model='basic')
+                                  weather_model='WeismanKlemp',
+                                  pressure_func=pressure_func)
+        print("Generated forcing.nc")
 
         self.init = tg.Topography(nz, nx, ny)
+        print("Generated init.nc")
 
 
 def main():
     # ICAR Options generate the ICAR namelist
-    options = opt.ICARoptions()
-    test = IdealTest(nz=40, nx=40, ny=40, n_hills=1.0)
+    nz = 40  # 32, good one for non-iter pressure
+    options = opt.ICARoptions(parcels=True, parcel_count=10,
+                              output_interval=60, nz=nz, hrs=6)
+    print("Generated icar_options.nml")
+    test = IdealTest(nz=nz, nx=40, ny=40, n_hills=1.0)
 
 if __name__ == "__main__":
     main()
