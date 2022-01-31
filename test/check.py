94accce924493d8f529f2c8dacb5c3f7a12e8794
Damian Rouson <rouson@lbl.gov>
2022-01-31 15:16:08 -0800

    feat(test): add check.py to compare output

---
+++ test/check.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import xarray as xr
+import sys
+import numpy as np
+
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
+# print(f"Comparing {filename1} with {filename2}")
+
+ds1 = xr.open_dataset(filename1)
+ds2 = xr.open_dataset(filename2)
+
+max_delta = 0
+for v in ds1.variables:
+    if v != "time":
+        delta = np.abs(ds1[v] - ds2[v]).values.max()
+        max_delta = max(delta, max_delta)
+# print(f"Maximum difference={max_delta}")
+
+print(f"{max_delta}")
