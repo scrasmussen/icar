addd2140647a66692f9029421e7396eff75d2a61
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-02-08 11:48:26 -0700

    Better organization of ICAR options generation with more options added. Will allow better fine tuning of generation of files. Cleaning up gen_ideal_test.py to take advantage the better python modules

--- tests/gen_ideal_test.py
+++ tests/gen_ideal_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from netCDF4 import Dataset
 import numpy as np
 import math
@@ -8,35 +9,46 @@ import Topography as tg
 import Forcing as fc
 import ICARoptions as opt
 
-# Python program generates an ideal case
-class IdealTest:
-    # from ideal test
-    sealevel_pressure = 100000.0 # pressure at sea level [Pa]
-    dz_value          = 500.0    # thickness of each model gridcell   [m]
-    # hill values currently do nothing
-    hill_height       = 1000.0   # height of the ideal hill(s) [m]
-    n_hills           = 1.0      # number of hills across the domain
-
-    def __init__(self, nz=10, nx=2, ny=2, n_hills=1.0):
-        rh = 0.9
-        u_test_val = 0.5
-        v_test_val = 0.5
-        w_test_val = 0.0
-        water_vapor_test_val = 0.001
-        theta_test_val = 300.0
-
-        self.forcing = fc.Forcing(nz, nx, ny, self.sealevel_pressure,
-                                  rh, u_test_val, v_test_val, w_test_val,
-                                  water_vapor_test_val, theta_test_val,
-                                  dz_value=self.dz_value)
-
-        self.init = tg.Topography(nz, nx, ny)
+# from ideal test
+sealevel_pressure = 100000.0 # pressure at sea level [Pa]
+dz_value          = 500.0    # thickness of each model gridcell   [m]
+# hill values currently do nothing
+hill_height       = 1000.0   # height of the ideal hill(s) [m]
+n_hills           = 1.0      # number of hills across the domain
+mixing_ratio = 0.00 # water vapor # not if constant
+# relative_humidity = 0.01
+u_test_val = v_test_val = w_test_val = 0.0
+water_vapor_test_val = 0.000
+theta_test_val = 300.0
+qv_val = mixing_ratio
 
 
 def main():
+    # choose dimensions
+    nz = 32
+    nx = ny = 40
+
     # ICAR Options generate the ICAR namelist
-    options = opt.ICARoptions()
-    test = IdealTest(nz=40, nx=40, ny=40, n_hills=1.0)
+    opt.ICARoptions(nz=nz, output_vars=['pressure','temperature'])
+    print("Generated icar_options.nml")
+
+    tg.Topography(nz, nx, ny)
+    print("Generated init.nc")
+
+    # --- choose function for creating pressure ---
+    pressure_func = 'calc_pressure_from_sea'
+    # pressure_func = 'calc_pressure_dz_iter'
+    # pressure_func = 'calc_pressure_1m_iter'
+
+    # double check all passed variable get used
+    forcing = fc.Forcing(nz, nx, ny, sealevel_pressure,
+                         u_test_val, v_test_val, w_test_val,
+                         water_vapor_test_val, theta_test_val,
+                         dz_value=dz_value, qv_val=qv_val,
+                         # weather_model='basic')
+                         weather_model='WeismanKlemp',
+                         pressure_func=pressure_func)
+    print("Generated forcing.nc")
 
 if __name__ == "__main__":
     main()
