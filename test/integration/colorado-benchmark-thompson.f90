0fe39dbe0508a16092c4a855e0cb02a391f47616
Damian Rouson <rouson@lbl.gov>
2022-03-10 13:27:20 -0800

    test(colorado): download/unzip/check output
    This commit adds a capability in the colorado-benchmark-thompson
    integraiton test to verify matching output relative to a saved
    version of the output

--- test/integration/colorado-benchmark-thompson.f90
+++ test/integration/colorado-benchmark-thompson.f90
@@ -8,6 +8,33 @@ program colorado_benchmark_thompson
 
   type(domain_t) domain
 
+  character(len=*), parameter :: restart_file="icar_gcm_test/restart/icar_rst_000001_2000-10-02_00-00-00.nc"
+
   call icar(domain)
-  
+
+  if (this_image()==1) then
+
+    call execute_command_line( &
+      command = "wget https://www.dropbox.com/s/xrhzec09yyaiui0/colorado_benchmark_thompson_restart.nc.zip"  , &
+      wait = .true., &
+      exitstat = exit_status &
+    )
+    call assert(exit_status==0,"colorado_benchmark_thompson: wget command")
+      
+    call execute_command_line( &
+      command = "unzip colorado_benchmark_thompson_restart.nc.zip"  , &
+      wait = .true., &
+      exitstat = exit_status &
+    )
+    call assert(exit_status==0,"colorado_benchmark_thompson: unzip command")
+      
+    call execute_command_line( &
+      command = "check.py " // restart_file // "colorado_benchmark_thompson_restart.nc"  , &
+      wait = .true., &
+      exitstat = exit_status &
+    )
+    call assert(exit_status==0, "colorado_benchmark_thompson: check.py")
+
+  end if
+
 end program
