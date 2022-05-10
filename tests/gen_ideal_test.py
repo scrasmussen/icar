54b81ca284d59622becb96122a73e38c9770c6d8
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-05-10 15:33:00 -0700

    Cleaning up test generator files

--- tests/gen_ideal_test.py
+++ tests/gen_ideal_test.py
@@ -3,7 +3,7 @@ from netCDF4 import Dataset
 import numpy as np
 from sys import exit, path
 from os import getcwd
-path.insert(0, getcwd()+'/../helpers/genNetCDF')
+path.insert(0, getcwd()+'/../helpers/generateTestFiles')
 import Topography as tg
 import Forcing as fc
 import ICARoptions as opt
@@ -17,22 +17,18 @@ nx = ny = 40
 
 # from ideal test
 dz_value          = 500.0    # thickness of each model gridcell   [m]
-# hill values currently do nothing
+# hill values currently do nothing, but one hill is created
 # hill_height       = 1000.0   # height of the ideal hill(s) [m]
 # n_hills           = 1.0      # number of hills across the domain
 
-# relative_humidity = 0.01
 u_test_val = v_test_val = w_test_val = 0.0
-water_vapor_test_val = 0.000
-mixing_ratio = 0.00 # water vapor # not if constant
-qv_val = mixing_ratio
+qv_val = 0.0001
 
 # --- choose function for creating pressure ---
+# options: calc_pressure_from_sea, calc_pressure_dz_iter, calc_pressure_1m_iter
 pressure_func = 'calc_pressure_from_sea'
-# pressure_func = 'calc_pressure_dz_iter'
-# pressure_func = 'calc_pressure_1m_iter'
 # --- choose weather model ---
-# weather_model = 'basic'
+# options: basic, WeismanKlemp
 weather_model = 'WeismanKlemp'
 
 
@@ -49,7 +45,6 @@ def main():
                          u_val=u_test_val,
                          v_val=v_test_val,
                          w_val=w_test_val,
-                         water_vapor_val=water_vapor_test_val,
                          dz_value=dz_value,
                          qv_val=qv_val,
                          weather_model=weather_model,
