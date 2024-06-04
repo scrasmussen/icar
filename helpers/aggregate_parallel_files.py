ff80fdfc54514125c0ce428452c80c496bb296db
Soren Rasmussen <s.c.rasmussen@gmail.com>
2024-06-04 12:25:05 -0600

    switching prefix back to file_search string

--- helpers/aggregate_parallel_files.py
+++ helpers/aggregate_parallel_files.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import glob
+import re
 import time
 # import multiprocessing as mp
 
@@ -9,14 +10,10 @@ import xarray as xr
 
 import sys
 
+# global variables
 pool = None
-
 no_restarted_from_s = 'No restarted_from Attribute'
-
-# This should be an input, this is the search string that is assumed to match
-# the output files to be aggregated.
-prefix = 'icar_out_'
-file_search = prefix+'{ens}_*'
+date_search_s = '[1-9][0-9][0-9][0-9]-[0-2][0-9]-[0-3][0-9]_[0-9][0-9]-[0-9][0-9]-[0-9][0-9]'
 
 
 def load_file(file_name):
@@ -32,6 +29,7 @@ def get_dims(dataset, section="d"):
             results.append(int(dataset.attrs[axis + section + position]))
     return results
 
+
 def get_dim_offset(dims):
     '''Return x_offset, y_offset
     For the staggered dims, offset=1, otherwise offset=0'''
@@ -42,6 +40,7 @@ def get_dim_offset(dims):
     if 'lat_v' in dims: y_off = 1
 
     return x_off, y_off
+
 
 def set_up_dataset(d):
     '''Create a dataset to cover the entire domain with the variables present in d
@@ -162,7 +161,8 @@ def agg_file(first_file, verbose=True):
     print(outputfile)
     data_set.to_netcdf(outputfile)
 
-def find_aggregate_from_date():
+
+def find_aggregate_from_date(file_search):
     '''
     Finds the date of the last aggregated file based on a specific prefix and date format.
 
@@ -171,23 +171,25 @@ def find_aggregate_from_date():
     '''
     first_files = glob.glob(file_search.format(ens="000001"))
     if not first_files:
-        print("Exiting: no output files found with prefix", prefix)
+        print("Exiting: no output files matching", file_search.format(ens="000001"))
         sys.exit()
     first_files.sort()
 
     # following 2000-01-01_00-00-00 format
-    find_s = prefix + '[0-9][0-9][0-9][0-9]-[0-2][0-9]-[0-3][0-9]_[0-9][0-9]-[0-9][0-9]-[0-9][0-9].nc'
-    first_agg_files = glob.glob(find_s)
+    find_agg_s = file_search.format(ens=date_search_s)
+    first_agg_files = glob.glob(find_agg_s)
     # if there are no aggregated files
     if not first_agg_files:
         return False #first_files[0]
 
     last_agg_file = first_agg_files[-1]
-    last_agg_file_date = last_agg_file.replace(prefix, '').replace('.nc','')
+    print("last_agg_file", last_agg_file)
+
+    last_agg_file_date = re.findall(date_search_s, last_agg_file)[0]
     return last_agg_file_date
 
 
-def get_restart_from_date(file_date):
+def get_restart_from_date(file_search, file_date):
     '''
     Gets the value of the attribute 'restarted_from' from an output file based on a given date.
 
@@ -198,7 +200,8 @@ def get_restart_from_date(file_date):
         str: The date from the 'restarted_from' attribute if the run was restarted, otherwise
              'No restarted_from Attribute' or 'Not Restarted'
     '''
-    out_filename = prefix + '000001_' + file_date + '.nc'
+    out_filename = file_search.replace('*','').format(ens='000001_') + file_date + '.nc'
+
     ds = xr.open_dataset(out_filename)
     try:
         restarted_from = ds.attrs['restarted_from']
@@ -208,7 +211,7 @@ def get_restart_from_date(file_date):
     return restarted_from
 
 
-def aggregate_prep():
+def aggregate_prep(file_search):
     '''
     Prepares aggregated files based on the current state of the output.
 
@@ -221,16 +224,16 @@ def aggregate_prep():
     Returns:
         None
     '''
-    find_agg_s = prefix + '[0-9][0-9][0-9][0-9]-[0-2][0-9]-[0-3][0-9]_[0-9][0-9]-[0-9][0-9]-[0-9][0-9].nc'
+    find_agg_s = file_search.format(ens=date_search_s)
 
     # find the last aggregated file, if no aggregated files, no prep needed
-    agg_from_date = find_aggregate_from_date()
+    agg_from_date = find_aggregate_from_date(file_search)
     if agg_from_date == False:
         print("No aggregated files")
         return
 
     # check the current output's restarted-from date and get list of aggregated files
-    restarted_from = get_restart_from_date(agg_from_date)
+    restarted_from = get_restart_from_date(file_search, agg_from_date)
     agg_files = glob.glob(find_agg_s)
     agg_files.sort()
 
@@ -259,7 +262,7 @@ def aggregate_prep():
             os.remove(f)
 
 
-def main(file_search = "icar_out_{ens}_*"):
+def main(file_search = "icar_out_{ens}*"):
     first_files = glob.glob(file_search.format(ens="000001"))
     first_files.sort()
 
@@ -267,7 +270,7 @@ def main(file_search = "icar_out_{ens}_*"):
     #  would map_async be faster for some reason?  I assume map is still parallel.
     # pool.map(agg_file, first_files)
 
-    aggregate_prep()
+    aggregate_prep(file_search)
 
     for f in first_files:
         agg_file(f)
@@ -285,7 +288,7 @@ def continuous(file_search):
         None
     '''
     print("Running continuous aggregation, Ctrl-C to stop")
-    aggregate_prep()
+    aggregate_prep(file_search)
 
     while True:
         first_files = glob.glob(file_search.format(ens="000001"))
